68c516a190f4ba1c6204d3a3324a1d6c6f7e9178
동영 <57888678+dahli4@users.noreply.github.com>
2024-10-04 11:34:32 +0900

    241004-reducer

--- react-reducer-example/src/App.js
+++ react-reducer-example/src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useReducer, useState } from 'react';
 function App() {
+  // const [count, setCount] = useState(0);
+  const [amount, amountDispatch] = useReducer(countAmount, 0);
+  const [plusNumber, setPlusNumber] = useState(5);
+  const [subNumber, setSubNumber] = useState(5);
+
+  function countAmount(oldAmount, action) {
+    if (action.type === "DOWN") {
+      return (oldAmount - action.minus);
+    }
+    else if (action.type === "RESET") {
+      return 0;
+    }
+    else if (action.type === "UP") {
+      return (oldAmount + action.plus);
+    }
+  }
+
+  function down() {
+    amountDispatch({ type: 'DOWN', plus: plusNumber, minus: subNumber });
+  }
+
+  function reset() {
+    amountDispatch({ type: 'RESET', number: plusNumber, minus: subNumber });
+  }
+
+  function up() {
+    amountDispatch({ type: 'UP', plus: plusNumber, minus: subNumber });
+  }
+
+  function changePlusNumber(event) {
+    setPlusNumber(Number(event.target.value));
+  }
+
+  function changeSubNumber(event) {
+    setSubNumber(Number(event.target.value));
+  }
+
   return (
     <div align='center'>
-      <input type='button' value='-' />
-      <input type='button' value='0' />
-      <input type='button' value='+' />
-      <p><span>0</span></p>
+      <input type='text' value={subNumber} size='3' onChange={changeSubNumber} />
+      <input type='button' value='-' onClick={down} />
+      <input type='button' value='0' onClick={reset} />
+      <input type='button' value='+' onClick={up} />
+      <input type='text' value={plusNumber} size='3' onChange={changePlusNumber} />
+      <p><span>{amount}</span></p>
     </div>
   );
 }
